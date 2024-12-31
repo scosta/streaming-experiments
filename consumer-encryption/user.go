0d0032d0412ea17ca42d2e8e60e60a1c93952d67
scosta <stevencosta@gmail.com>
2024-12-31 07:33:43 -0800

    first commit of consumer that can decrypt an encrypted field

---
+++ consumer-encryption/user.go
@@ -0,0 +1,10 @@
+package main
+
+type User struct {
+	FirstName        string `avro:"first_name"`
+	LastName         string `avro:"last_name"`
+	PhoneNumber      string `avro:"phone_number"`
+	Address          string `avro:"address"`
+	Age              int    `avro:"age"`
+	CreditCardNumber string `avro:"credit_card_number"`
+}
